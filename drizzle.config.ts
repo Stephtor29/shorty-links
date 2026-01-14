250ddea8a793a2cf742c3a1ea1e8ac056da28f1c
Stephtor29 <gisselletoro22@gmail.com>
2026-01-14 17:18:58 -0600

    update

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,13 @@
-import type { Config } from "drizzle-kit";
+import { defineConfig } from "drizzle-kit";
+import * as dotenv from "dotenv";
 
-export default {
+dotenv.config({ path: ".env.local" });
+
+export default defineConfig({
   schema: "./lib/schema.ts",
-  out: "./drizzle",
-  driver: "pg",
+  out: "./migrations",
+  dialect: "postgresql",
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL!,
   },
-} satisfies Config;
+});
